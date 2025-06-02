f9df40c85ea509dcade4291e57f17d8a699a54ba
Van Loi <ldzpro68@gmail.com>
2025-06-02 08:40:31 +0700

    fix: conflig classNames

--- components/molecules/Layout/Content.tsx
+++ components/molecules/Layout/Content.tsx
@@ -1,6 +1,6 @@
-import clsx from "clsx";
 import { LayoutProps } from "./Layout";
+import { twMerge } from "tailwind-merge";
 
 export function Content({ children, className = '' }: LayoutProps) {
-  return <main className={clsx('flex-1 px-4 py-6 bg-white', className)}>{children}</main>;
+  return <main className={twMerge('flex-1 px-4 py-6 bg-white', className)}>{children}</main>;
 }
--- components/molecules/Layout/Footer.tsx
+++ components/molecules/Layout/Footer.tsx
@@ -1,6 +1,6 @@
-import clsx from "clsx";
 import { LayoutProps } from "./Layout";
+import { twMerge } from "tailwind-merge";
 
 export function Footer({ children, className = '' }: LayoutProps) {
-  return <footer className={clsx('h-12 px-4 bg-gray-100 flex items-center', className)}>{children}</footer>;
+  return <footer className={twMerge('h-12 px-4 bg-gray-100 flex items-center', className)}>{children}</footer>;
 }
